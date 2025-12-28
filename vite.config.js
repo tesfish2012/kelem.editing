2de32371702676e17d45c0792202073b26785def
tesfish2012 <tesfay3814@gmail.com>
2025-12-28 13:03:54 +0300

    updates

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,16 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-
+  base: '/', // ensures correct paths on Render
   server: {
     host: '0.0.0.0'
   }
-  
-}) 
- 
+})

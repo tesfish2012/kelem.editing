48bb3445b3195f53a01ceb8d25921deb18a8b01e
tesfish2012 <tesfay3814@gmail.com>
2025-12-28 12:33:36 +0300

    updates

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,22 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-
-  server: {
-    host: '0.0.0.0'
-  }
   
-}) 
- 
+  // optional: dev server fallback
+  server: {
+    host: '0.0.0.0',
+    historyApiFallback: true // ensures /login etc work locally
+  },
+
+  // optional: set base if deploying to a subfolder
+  base: '/'
+})

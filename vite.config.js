dc8e4d93bfa3e214b3d3f867457499a695e88d8f
tesfish2012 <tesfay3814@gmail.com>
2025-12-28 13:09:10 +0300

    updates

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,22 @@
 import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-
+  plugins: [
+    vue(),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
 
-  // Base URL for deployment (important for Render)
-  base: '/',
-
   server: {
-    host: '0.0.0.0',
-    port: 5173, // optional, default is 5173
-  },
-
-  build: {
-    outDir: 'dist', // output folder
-    emptyOutDir: true,
+    host: '0.0.0.0'
   }
-})
+  
+}) 
+ 
